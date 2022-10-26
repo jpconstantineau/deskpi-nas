4476b35d89f6366ec901d37fe671a9278ca2beb1
Pierre Constantineau <jpconstantineau@gmail.com>
2022-10-25 23:11:21 -0600

    fix link

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook update.yml -i inventory.ini  --key-file "~/.ssh/local-pi-id_ed25519.pub"
+ansible-playbook update.yml -i inventory.ini  --key-file "~/.ssh/local-pi-id_ed25519"
